a246a984eecad949a6c8e7d2e0c5252655456bfe
noelledons <noelledonkor@gmail.com>
2025-06-12 15:28:49 +0100

    update to url name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-portfolio-one/',
+  base: '/noelle-donkor-portfolio/',
   plugins: [react()],
   css: {
     modules: {
